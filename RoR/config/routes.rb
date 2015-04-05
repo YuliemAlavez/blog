dbaad375a9370d878b752a8a20a3287a5d0f3974
Yuliem D. Alavez García <yuliemalavez@outlook.com>
2015-04-05 05:31:44 -0500

    Ejecución de scaffold
    Se creo una estructura para la tabla que contendrá los post,
    posteriormente esta estructura fue trasladada a la base de datos.

--- RoR/config/routes.rb
+++ RoR/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
+  resources :posts
   get 'inicio/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
